5358ea18dba20a878e796c0aa799f64a5a7ec26f
Nada Malash <nadamalash58@gmail.com>
2023-09-30 23:33:06 +0300

    Created vpc file

--- variables.tf
+++ variables.tf
@@ -14,3 +14,39 @@ variable "environment" {
   description = "The environment of the project."
   type        = string
 }
+
+# VPC Variables
+variable "vpc_cidr" {
+  description = "VPC CIDR Block."
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "Public Subnet AZ1 CIDR Block."
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "Public Subnet AZ2 CIDR Block."
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "Private App Subnet AZ1 CIDR Block."
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "Private App Subnet AZ2 CIDR Block."
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "Private Data Subnet AZ1 CIDR Block."
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "Private Data Subnet AZ2 CIDR Block."
+  type        = string
+}
